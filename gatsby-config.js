809b2355a04b813dec4d9b8dd0adcbc9425e3d22
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-06-24 17:40:58 +1000

    feat: Add in Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-170614381-1`,
       },
     },
     {
